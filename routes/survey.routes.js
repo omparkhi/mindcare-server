c34c5e92277e76f595f38758da34d12247fbfd42
vitthal4545 <vitthalgs1434@gmail.com>
2025-09-14 14:14:35 +0530

    created route for get survey history by id

--- routes/survey.routes.js
+++ routes/survey.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  getSurveyById,
   getSurveyHistory,
   saveSurvey,
 } from "../controllers/survey.controller.js";
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/", authMiddleware, saveSurvey);
 router.get("/history", authMiddleware, getSurveyHistory);
+router.get("/history/:id", authMiddleware, getSurveyById);
 
 export default router;
