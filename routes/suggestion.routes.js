c735f39bc818e9303cf4eb3193a51f9fbd1e10e2
vitthal4545 <vitthalgs1434@gmail.com>
2025-09-15 20:00:33 +0530

    update the route for the survey form submittion and report generation

--- routes/suggestion.routes.js
+++ routes/suggestion.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { generateSuggestions } from "../controllers/suggestion.controller.js";
+import {
+  generateMentalHealthReport,
+  generateSuggestions,
+} from "../controllers/suggestion.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/generate", authMiddleware, generateSuggestions);
+router.post("/submit", authMiddleware, generateMentalHealthReport);
 
 export default router;
